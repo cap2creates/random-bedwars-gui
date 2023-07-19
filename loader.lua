17fbd2696d2d211063858b18256eb6efb9bc81fb
cap2creates <138265340+cap2creates@users.noreply.github.com>
2023-07-19 15:20:53 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,16 +5,17 @@ function load()
         "functions.lua",
         "gui.lua",
     }
+    local dothingname = "mainscript.lua"
     if (not isfolder("test")) then
         folder = makefolder("test")
-    else
-        folder = isfolder("test")
     end
     for number,filename in pairs(filenames) do
         if (not isfile("test/"..filename)) then
             writefile("test/"..filename, game:HttpGet("https://raw.githubusercontent.com/cap2creates/random-bedwars-gui/main/lua-test/"..filename, true))
         end
     end
+    local mainfile = readfile("test/"..dothingname)
+    mainfile.dothing()
     return folder
 end
 load()

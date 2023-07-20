c3e2eeb9dc4d6ab0e911e4d55f1ceb5f2c48ae42
cap2creates <138265340+cap2creates@users.noreply.github.com>
2023-07-20 10:47:35 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,9 @@
+function githubRequest(url)
+    if not isfile("test/"..url) then
+        writefile("test/"..url, loadstring(game:HttpGet("https://raw.githubusercontent.com/cap2creates/random-bedwars-gui/main/lua-test/"..url,true))())
+    end
+    return readfile("test/"..url)
+end
 function load()
     local folder
     local filenames = {
@@ -5,20 +11,19 @@ function load()
         "functions.lua",
         "gui.lua",
     }
+    local files = filenames
     local dothingname = "mainscript.lua"
     if (not isfolder("test")) then
         folder = makefolder("test")
     end
     for number,filename in pairs(filenames) do
         if (not isfile("test/"..filename)) then
-            writefile("test/"..filename, game:HttpGet("https://raw.githubusercontent.com/cap2creates/random-bedwars-gui/main/lua-test/"..filename, true))
+            local suc, err = pcall(function() githubRequest(filename) end)
+            if err then warn(err) else
+                files[filename] = suc
+            end
         end
     end
-    print("e")
-    local mainfile = require(readfile("test/"..dothingname))
-    if mainfile then print(mainfile) end
-    if not mainfile then print("fail") end
-    mainfile.dothing()
-    return folder
+    return {folder,files}
 end
 load()
